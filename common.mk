e811bc962527b637ada9f407c998b9c0913869e7
LibXZR <i@xzr.moe>
2022-05-26 20:37:08 +0800

    Revert "sm8250-common: Enable Android Beam"
    No longer works with aosp nfc.
    
    This reverts commit cc9fa2427234e4b634ccff96e5e725f6fc0ee517.
    
    Change-Id: I183e2ffd49328e1d92ce5d6ad26be90ff807d739

--- common.mk
+++ common.mk
@@ -399,10 +399,6 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
-# NFC (Android Beam)
-PRODUCT_COPY_FILES += \
-    vendor/kscope/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
-
 # OMX
 PRODUCT_PACKAGES += \
     init.qti.media.sh \
