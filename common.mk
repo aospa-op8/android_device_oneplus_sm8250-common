015447a929c99c270e293f6453c282feae52116c
LibXZR <i@xzr.moe>
2022-06-27 15:41:11 +0800

    sm8250-common: Build vendor.oneplus.hardware.camera from source
    Change-Id: I3f1904e5c280d1c9d8ec58af07ba42fdb3af2657

--- common.mk
+++ common.mk
@@ -200,6 +200,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     Snap \
+    vendor.oneplus.hardware.camera@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Charge control
